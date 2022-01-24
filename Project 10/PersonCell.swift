b6732ba9bc729215c9f3223ff9063555b7ef8898
Pablo2maker <pablo-nox1@hotmail.com>
2022-01-24 12:30:40 +0100

    Initial Commit

---
+++ Project 10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project 10
+//
+//  Created by Pablo Rodrigues on 20/01/2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
